9394d971b9dbf19d63d7b12f81491f224612ef65
tomprogramuje <tom.severa@outlook.cz>
2023-11-27 13:48:59 +0100

    basic validation implemented

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/tomprogramuje/card_validator/validation"
 )
 
+const port = ":80"
+
+type jsonRequest struct {
+	CreditCardNumber string `json:"creditCardNumber"`
+}
+
+type jsonResponse struct {
+	Valid bool `json:"valid"`
+	Issuer string `json:"issuer"`
+}
+
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	// checks if the http method is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// decodes json payload 
+	var reqBody jsonRequest
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+
+	// validates the card number
+	valid, issuer := validation.CheckCard(reqBody.CreditCardNumber)
+
+	// makes json response
+	resp := jsonResponse{Valid: valid, Issuer: issuer}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
+
 func main() {
-	cardNumber := "4539059644464842"
-	fmt.Println(validation.checkCard(cardNumber))
+	http.HandleFunc("/", handlePostRequest)
+
+	log.Println("Starting app on port", port)
+	err := http.ListenAndServe(port, nil)
+	log.Println(err)
 }
--- validation/validation.go
+++ validation/validation.go
@@ -2,41 +2,45 @@ package validation
 
 import "fmt"
 
-func checkCard(cardNumber string) string {
+func CheckCard(cardNumber string) (bool, string) {
 	var cardNumberArr []int
-	
-	// parsing card number string to int and appending it to a slice
+
+	// parses card number string to int and appending it to a slice
 	for _, n := range cardNumber {
 		cardNumberArr = append(cardNumberArr, int(n-'0'))
 	}
 
-	// iterating over slice from the end and multiplying every other number by two 
+	// iterates over slice from the end and multiplying every other number by two
 	for i := len(cardNumberArr) - 1; i >= 0; i-- {
-		if i % 2 == 0 {
+		if i%2 == 0 {
 			cardNumberArr[i] *= 2
 		}
 	}
 
-	// making every two digit number into single digit by adding the digits of the number together 
+	// makes every two digit number into single digit by adding the digits of the number together
 	for i := range cardNumberArr {
 		if cardNumberArr[i] > 9 {
 			cardNumberArr[i] -= 9
 		}
 	}
 
-	// summing all the digits of card number 
+	// summs all the digits of card number
 	sum := 0
 	for i := range cardNumberArr {
 		sum += cardNumberArr[i]
 	}
-
-	return isValid(sum)
+	fmt.Println(sum)
+	return isValid(sum), creditCardIssuer(cardNumber)
 }
 
-func isValid(cardNumberSum int) string {
-	if cardNumberSum % 10 == 0 {
-		return fmt.Sprintln("This card is valid")
-	} else {
-		return fmt.Sprintln("This card is not valid")
+func isValid(cardNumberSum int) bool {
+	return cardNumberSum%10 == 0
+}
+
+func creditCardIssuer(cardNumber string) string {
+	if cardNumber[0] == '4' {
+		return "Visa"
 	}
+
+	return "Unknown"
 }
